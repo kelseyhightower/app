66362ac069460494646b98a558f850ba14663191
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-04-14 15:54:56 -0700

    format time

--- handlers/logging.go
+++ handlers/logging.go
@@ -9,7 +9,8 @@ import (
 func LoggingHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		format := "%s - - [%s] \"%s %s %s\" %s\n"
-		fmt.Printf(format, r.RemoteAddr, time.Now(), r.Method, r.URL.Path, r.Proto, r.UserAgent())
+		fmt.Printf(format, r.RemoteAddr, time.Now().Format(time.RFC1123),
+			r.Method, r.URL.Path, r.Proto, r.UserAgent())
 		h.ServeHTTP(w, r)
 	})
 }
