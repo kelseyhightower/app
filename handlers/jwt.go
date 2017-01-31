33f20db22eb44639bd2554ee4cca9d8afcf691a9
Eric Muellenbach <emuellenbach@gmail.com>
2017-01-31 17:21:31 +0100

    Remove hardcoded string "secret" from jwt.go

--- handlers/jwt.go
+++ handlers/jwt.go
@@ -6,10 +6,10 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-func JWTAuthHandler(h http.HandlerFunc) http.HandlerFunc {
+func JWTAuthHandler(secret string, h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		token, err := jwt.ParseFromRequest(r, func(token *jwt.Token) (interface{}, error) {
-			return []byte("secret"), nil
+			return []byte(secret), nil
 		})
 		if err != nil || !token.Valid {
 			http.Error(w, "authorization failed", http.StatusUnauthorized)
